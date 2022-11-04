2fbf895c4287b56b9dc3644863aab7182eb2658c
Baiel <baielkarybaev@gmail.com>
2022-11-04 15:14:43 +0600

    content two

--- src/components/ComponentTwo.jsx
+++ src/components/ComponentTwo.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "../Css/Two.css";
 import imgsun from "../imgs/Rectangle 7.jpg";
 import homeimg from "../imgs/Rectangle 9.jpg"
+import imghome from "../imgs/Rectangle 8.jpg"
 
 export default function ContentTwo() {
     return (
@@ -21,46 +22,52 @@ export default function ContentTwo() {
                 <img src={imgsun} />
             </div>
             <div className="bi-two">
-                <div className="textTwo">
-                    <h3>ЗЕЛЕНЫЙ ТАРИФ</h3>
-                    <ul>
-                        <li><a href="#">продажа</a></li>
-                        <li><a href="#">закрепление</a></li>
-                        <li><a href="#">ограничения</a></li>
-                    </ul>
-                    <div className="bi-tree">
-                        <img src={homeimg}/>
-                        <div className="bi-four">
-                            <p>Цена, по которой домохозяйства могут продать электроэнергию, более чем в 10 раз превышает цену, по которой они ее покупают для своих нужд — 0,16 Евро/кВт (продажа).</p>
-                            <div className="arrow">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="56" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 26 16">
-                                    <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
-                                </svg>
-                                <svg xmlns="http://www.w3.org/2000/svg" width="56" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
-                                    <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
-                                </svg>
+                <div className="bi-big">
+                    <div className="textTwo">
+                        <h3>ЗЕЛЕНЫЙ ТАРИФ</h3>
+                        <ul>
+                            <li><a href="#">продажа</a></li>
+                            <li><a href="#">закрепление</a></li>
+                            <li><a href="#">ограничения</a></li>
+                        </ul>
+                        <div className="bi-tree">
+                            <img src={homeimg} alt="jome" />
+                            <div className="bi-four">
+                                <p>Цена, по которой домохозяйства могут продать электроэнергию, более чем в 10 раз превышает цену, по которой они ее покупают для своих нужд — 0,16 Евро/кВт (продажа).</p>
+                                <div className="arrow">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="56" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 26 16">
+                                        <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                                    </svg>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="56" height="16" fill="currentColor" class="bi bi-arrow-right" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M1 8a.5.5 0 0 1 .5-.5h11.793l-3.147-3.146a.5.5 0 0 1 .708-.708l4 4a.5.5 0 0 1 0 .708l-4 4a.5.5 0 0 1-.708-.708L13.293 8.5H1.5A.5.5 0 0 1 1 8z" />
+                                    </svg>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <div className="bi-six">
+                        <div>
+                            <h3>Гарантия выкупа</h3>
+                            <p>Гарантированный выкуп электроэнергии государством - прописано в НКРЕ №447 </p>
+                        </div>
+                        <div>
+                            <h3>Долгосрочно</h3>
+                            <p>это проект, поддерживаемый международными инвестиционными организациями</p>
+                        </div>
+                        <div>
+                            <h3>Вид вложения</h3>
+                            <p>Альтернатива банковским депозитам, окупаемость - от 16% до 18% годовых</p>
+                        </div>
+                        <div>
+                            <h3>Высокая ставка</h3>
+                            <p>Самая высокая тарифная ставка в европе - 0,16 евро</p>
+                        </div>
+                    </div>
                 </div>
-               <div className="bi-six">
-                    <div>
-                        <h3>Гарантия выкупа</h3>
-                        <p>Гарантированный выкуп электроэнергии государством - прописано в НКРЕ №447 </p>
-                    </div>
-                    <div>
-                        <h3>Долгосрочно</h3>
-                        <p>это проект, поддерживаемый международными инвестиционными организациями</p>
-                    </div>
-                    <div>
-                        <h3>Вид вложения</h3>
-                        <p>Альтернатива банковским депозитам, окупаемость - от 16% до 18% годовых</p>
-                    </div>
-                    <div>
-                        <h3>Высокая ставка</h3>
-                        <p>Самая высокая тарифная ставка в европе - 0,16 евро</p>
-                    </div>
-               </div>
+                <div className="bi-ci">
+                    <p className="ci-text">«Зеленый тариф» — это особые тарифные условия, которые позволяют частным домохозяйствам  не только потреблять, но и продавать выработанное домашними солнечными электростанциями электричество в электросеть общего пользования.</p>
+                    <img src={imghome} alt="home"/>
+                </div>
             </div>
         </div>
     )
